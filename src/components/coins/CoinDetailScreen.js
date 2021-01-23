598ed018bcc7780d799143cf77818eac6157f714
Miguel Rivera Robles <al221910331@gmail.com>
2021-01-23 09:30:29 -0600

    CoinDetailScreen

---
+++ src/components/coins/CoinDetailScreen.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {View, Text} from 'react-native';
+
+//declaramos nuestro componente tipo class
+
+class CoinsDetailScreen extends Component {
+    render(){
+        return (
+            <view>
+                <Text>
+                    Mi Primer Pantalla detail en react native
+                </Text>
+            </view>                           
+
+        )
+    }
+}
+
+export default CoinsDetailScreen;
