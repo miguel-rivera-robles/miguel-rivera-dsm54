55aed26e787e967919e10e19668924fbae54824b
Miguel Rivera Robles <al221910331@gmail.com>
2021-03-18 01:23:10 -0600

    FlexBox

--- App.js
+++ App.js
@@ -6,42 +6,45 @@
  * @flow strict-local
  */
 
+//import CoinStack from './src/components/coins/CoinStack';
+//import { color } from 'react-native-reanimated';
 import React from 'react';
-import CoinStack from './src/components/coins/CoinStack';
 import {NavigationContainer} from '@react-navigation/native';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import {View, Text, StyleSheet } from 'react-native';
-import { color } from 'react-native-reanimated';
-//import Icon from 'react-native-vector-icons/FontAwesome';
-/* Una funcion para que sea nuestro componente de home screen */
+import Flex from './src/components/Flex';
 
+
+/* Una funcion para que sea nuestro componente de home screen */
 function homeScreen() {
   return(
-    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor: '#F9278D'}}>
-      <Text>Screen de Inicio</Text>
-    </View>
+        <Flex />
   )
 }
-/* Una funcion para que sea nuestro componente de home screen */
 
+
+/* Una funcion para que sea nuestro componente de Categories screen */
 function categoriesScreen() {
   return(
-    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor:'#9D30F7'}}>
-      <Text>Screen de Categorias</Text>
+    <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor:'#ebd8b7'}}>
+    <Text>Screen de Categorias</Text>
     </View>
   )
 }
+
+
+/* Una funcion para que sea nuestro componente de Post screen */
 function postsScreen() {
   return(
     <View style={{flex: 1,justifyContent: 'center',alignItems: 'center', backgroundColor:'#78CFB4'}}>
-      <Text>Screen de Posts</Text>
+    <Text>Screen de Posts</Text>
     </View>
   )
 }
 
 const Tab=createBottomTabNavigator();
-//const myIcon = <Icon name="rocket" size={30} color="#900" />;
+
 const App: () => React$Node = () => {
   return (
     <>
@@ -49,27 +52,30 @@ const App: () => React$Node = () => {
         <Tab.Navigator
           initialRouteName="Home"
           tabBarOptions={{
-            activeTintColor: '#e91e63',
+            activeTintColor: '#070606',
+            labelStyle:{
+            fontSize:15
+          },
           }}
         >
           <Tab.Screen name="Home" component={homeScreen}
             options={{
               tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="home" color={'#F32A64'} size={25} />
+                <MaterialCommunityIcons name="home" color={'#6e7c7c'} size={25} />
               ),
             }}
             />
           <Tab.Screen name="Categories" component={categoriesScreen}
             options={{
               tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="cards-variant" color={'#F32A64'} size={25} />
+                <MaterialCommunityIcons name="cards-variant" color={'#6e7c7c'} size={25} />
               ),
             }}
           />
           <Tab.Screen name="Posts" component={postsScreen}
              options={{
               tabBarIcon: ({ color, size }) => (
-                <MaterialCommunityIcons name="bookmark-multiple-outline" color={'#F32A64'} size={25} />
+                <MaterialCommunityIcons name="bookmark-multiple-outline" color={'#435560'} size={25} />
               ),
             }}
           />
